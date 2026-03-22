0ec1ff1afc2005c89e887f12fbf2161e27773f1d
RasmusLumholdt <dev.rlumhold@gmail.com>
2026-03-22 11:20:22 +0100

    added git ssh key

--- home.nix
+++ home.nix
@@ -103,6 +103,19 @@ programs.git = {
   };
 };
 
+programs.ssh = {
+  enable = true;
+  enableDefaultConfig = false;
+
+  matchBlocks."github.com" = {
+    hostname = "github.com";
+    user = "git";
+    identityFile = [
+      "~/.ssh/id_ed25519"
+    ];
+    addKeysToAgent = "yes";
+  };
+};
 
   programs.bash = {
     enable = true; 
