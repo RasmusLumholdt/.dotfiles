4d794a7f73dcb5b89a2fa1188980f9e190b5eada
RasmusLumholdt <dev.rlumhold@gmail.com>
2026-05-11 11:32:53 +0200

    yeet

--- modules/services/audioBookshelf.nix
+++ modules/services/audioBookshelf.nix
@@ -3,11 +3,12 @@
 { self, inputs, ... }: {
   flake.nixosModules.audioBookshelf = { config, pkgs, ... }: {
   
-  services.audiobookshelf = {
-    enable = true;
-    user = "audiobookshelf";
-    group = "audiobookshelf";
-    dataDir = "/var/lib/audiobookshelf";
-    port = 13378;
+      services.audiobookshelf = {
+        enable = true;
+        user = "audiobookshelf";
+        group = "audiobookshelf";
+        dataDir = "/var/lib/audiobookshelf";
+        port = 13378;
+      };
   };
 }
